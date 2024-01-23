04f1dbb92edba163b88dfc3aa2f9dd471eacd38c
Divyank Raja <107948268+RajaDivyank@users.noreply.github.com>
2024-01-23 23:29:43 +0530

    api

---
+++ API Create and Consume/APIConsume/APIConsume/Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIConsume.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult PersonView()
+        {
+            return View();
+        }
+    }
+}
---
+++ API Create and Consume/APIConsume/APIConsume/Models/PersonModel.cs
@@ -0,0 +1,10 @@
+﻿namespace APIConsume.Models
+{
+    public class PersonModel
+    {
+        public int PersonId { get; set; }
+        public string? PersonName { get; set; }
+        public string? PersonContact { get; set; }
+        public string? PersonEmail { get; set; }
+    }
+}
